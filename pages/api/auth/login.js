faf313f6a9a7d81dd55b1978d176cbb8c0c15b2c
musa <musa@chb-technologies.ch>
2023-09-26 15:25:55 +0100

    done-with-login-endpoint

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,6 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' })
-  }
-  
+import dbConnect from "@/dbConnect";
+import User from "@/schema/User";
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+
+export default async function handler(req, res) {
+  const key = process.env.SECRET_KEY || "secret_key";
+
+  await dbConnect()
+  const { email, password } = req.body
+  await User.findOne({ email })
+    .then(async (user) => {
+      console.log("log")
+      if (user) {
+        if (bcrypt.compareSync(password, user.password)) {
+          const token = jwt.sign({
+            userId: user._id,
+            email: user.email,
+            username: user.username
+          }, key)
+          res.status(201).json({ token, message: "Login successful" })
+        }
+      }
+      else {
+        res.status(401).json({
+          message: "User does not exist",
+        });
+      }
+    })
+}
