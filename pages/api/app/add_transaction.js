ca4b53d92336051b874f3508ab42062fb627fe4e
musa <musa@chb-technologies.ch>
2023-09-26 16:43:53 +0100

    done with creating transaction endpoint

--- pages/api/app/add_transaction.js
+++ pages/api/app/add_transaction.js
@@ -1,6 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' })
+import dbConnect from "@/dbConnect";
+import Transaction from "@/schema/Transaction";
+import jwt from "jsonwebtoken"
+
+export default async function handler(req, res) {
+  await dbConnect()
+console.log(req.cookies, req.headers['authorization'])
+  const decode = jwt.verify(req.headers['authorization'], "secret_key")
+
+  const {
+    category,
+    amount,
+    date,
+    description
+  } = req.body;
+  await Transaction.create({category, amount, date, description, userId: decode.userId})
+  .then((transaction)=>{
+    res.status(201).json({ data: transaction })
+  })
   }
   
