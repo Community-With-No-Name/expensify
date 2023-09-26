81bf6d55b77ca48953a518b8a445f3647afa1c73
Francis matthew <matthewabiodun2001@gmail.com>
2023-09-26 11:12:14 +0100

    password recovery pages done

---
+++ pages/auth/recovery2.jsx
@@ -0,0 +1,20 @@
+import InputComp from "@/components/shared/input";
+import React from "react";
+import { FaArrowLeftLong } from "react-icons/fa6";
+
+export default function Recovery() {
+  return (
+    <div className=" pt-[66px] px-6 bg-[#EDEEF2] h-screen w-screen">
+      <div className="flex  gap-[30px] items-center mb-[294px] ">
+        <FaArrowLeftLong className="h-6 w-7 text-[#E59500] "></FaArrowLeftLong>
+        <div className="text-semibold h-[29px] text-[19px] w-screen text-[#1466B8] ">
+          Password Recovery
+        </div>
+      </div>
+      <div className=" leading-5 text-center h-4 font-semibold  text-base">
+        Please check your email and follow the instructions to reset your
+        password.
+      </div>
+    </div>
+  );
+}
