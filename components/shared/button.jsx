3ea673354ee387d524b8ad7df5c2a38983c6c625
musa <musa@chb-technologies.ch>
2023-09-26 15:01:02 +0100

    done with the reset password page

--- components/shared/button.jsx
+++ components/shared/button.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function Button({ color, name, click }) {
   return (
-    <div>
-      <button className={` w-full py-3 rounded-lg ${color === 'primary' ? 'bg-[#7339AC]' : 'bg-[#E59500]'}`} onClick={click}>{name}</button>
-    </div>
+      <button className={` w-full py-3 rounded-lg text-white ${color === 'primary' ? 'bg-[#7339AC]' : 'bg-[#E59500]'}`} onClick={click}>{name}</button>
   )
 }
--- components/shared/input.jsx
+++ components/shared/input.jsx
@@ -2,13 +2,11 @@ import React from "react";
 
 export default function InputComp({ placeholder, type, required }) {
   return (
-    <div className="flex justify-center items-center w-full">
       <input
         placeholder={placeholder}
         type={type}
         required={required}
-        className=" h-12 border-2 w-full border-blue-600 rounded-md m-4 pl-3 shadow-lg hover:shadow-xl hover:border-black"
+        className="w-full h-12 pl-3 border-2 border-blue-600 rounded-md shadow-lg outline-none hover:shadow-xl"
       />
-    </div>
   );
 }
