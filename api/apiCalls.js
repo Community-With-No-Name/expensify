4ea821c4e2396f741d7c82ef43923d65795484f8
Jubril Musa <41811898+musajubril@users.noreply.github.com>
2023-09-27 15:18:33 +0100

    added api folder and contents

---
+++ api/apiCalls.js
@@ -0,0 +1,20 @@
+import apiToken from './apiToken'
+import axios from "axios"
+
+export const postRequest = async ({ url, data } ) => {
+    const response = await apiToken.post(url, data)
+    return response.data
+  }
+  
+  
+  export const getRequest = async ({ url }) => {
+    const response = await apiToken.get(url)
+    return response.data
+  }
+
+  export const login = async ({url, data}) => {
+    const res = await axios.post(url, data)
+  }
+  export const register = async ({url, data}) => {
+    const res = await axios.post(url, data)
+  }
---
+++ api/apiToken.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-param-reassign */
+import axios from 'axios'
+
+const apiToken = axios.create({
+  baseURL: process.env.REACT_APP_BACKEND_API_URL
+})
+
+apiToken.interceptors.request.use(
+  (config) => {
+    // Add authorization key to config object if it exist
+    const ex_pensify_token = localStorage.getItem('ex_pensify_token')
+    if (ex_pensify_token) {
+      config.headers.Authorization = `${ex_pensify_token}`
+    }
+
+    return config
+  },
+  (error) => Promise.reject(error)
+)
+
+export default apiToken
