c3e8361cf30ad72712c6b7fc3dd86d327233f474
Jubril Musa <41811898+musajubril@users.noreply.github.com>
2023-09-27 16:17:11 +0100

    added hydration

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,16 @@
 import '@/styles/globals.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Dosis } from 'next/font/google';
+import { Hydrate } from "@tanstack/react-query/hydration";
 
 const dosis = Dosis({ subsets: ["latin"] })
 const queryClient = new QueryClient()
 export default function App({ Component, pageProps }) {
   return <QueryClientProvider client={queryClient}>
+    <Hydrate state={pageProps.dehydratedState}>
   <div className={`${dosis.className} max-w-sm mx-auto`}> 
   <Component {...pageProps} />
   </div>
+    </Hydrate>
   </QueryClientProvider>
 }
