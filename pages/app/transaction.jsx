e85901f0adb3ac9a6d3320d4bb7a2b60eaf40220
Francis matthew <matthewabiodun2001@gmail.com>
2023-10-12 11:53:06 +0100

    filtered transaction by expenses and income done

--- pages/app/transaction.jsx
+++ pages/app/transaction.jsx
@@ -1,26 +1,41 @@
 import { AiOutlineArrowLeft } from "react-icons/ai";
-import Items from '@/components/transaction screen/items';
-import Drop from '@/components/transaction screen/dropdown';
-import Menu from '@/components/transaction screen/menu';
+import Items from "@/components/transaction screen/items";
+import Drop from "@/components/transaction screen/dropdown";
+import Menu from "@/components/transaction screen/menu";
 import { useQuery } from "@tanstack/react-query";
 import queryKeys from "@/api/queryKeys";
 import { getRequest } from "@/api/apiCalls";
 import { GET_TRANSACTION } from "@/api/apiURL";
-import React from 'react';
+import React from "react";
 
 export default function transaction() {
-  const [transactions, setTransactions] = React.useState([])
- const {data} = useQuery({
-  queryKey:[queryKeys.getTransactions], 
-  queryFn: async()=> await getRequest({url:GET_TRANSACTION})
- })
- React.useEffect(()=>{
-  setTransactions(data?.data)},[data])
- console.log(transactions, data);
-  return (
-    <div className=' p-11'>
+  const [transactions, setTransactions] = React.useState([]);
 
-<div className=" flex items-center pb-7">
+  const categoriseTransactions = (allTransactions) => {
+    const expenses = allTransactions.filter(
+      (transaction) => transaction.type === "expense"
+    );
+    const income = allTransactions.filter(
+      (transaction) => transaction.type === "income"
+    );
+    return { expenses, income };
+  };
+  const { data } = useQuery({
+    queryKey: [queryKeys.getTransactions],
+    queryFn: async () => await getRequest({ url: GET_TRANSACTION }),
+  });
+  React.useEffect(() => {
+    if (data && data.data){
+      const allTransactions = data.data;
+      const categorisedTransactions = categoriseTransactions(allTransactions);
+      setTransactions(categorisedTransactions);
+    }
+    setTransactions(data?.data);
+  }, [data]);
+  console.log(transactions, data);
+  return (
+    <div className=" p-11">
+      <div className=" flex items-center pb-7">
         <AiOutlineArrowLeft className=" justify-start text-xl" />
 
         <div className=" flex justify-center w-screen">
@@ -28,21 +43,22 @@ export default function transaction() {
             Login
           </h1>
         </div>
-
       </div>
 
-<div className=' flex gap-6'>
-      <Drop/>
+      <div className=" flex gap-6">
+        <Drop />
 
-      <Menu/>
-
+        <Menu />
       </div>
-      <div className=' flex flex-col'> 
-      {
-        transactions?.map(trans =>(
-          <Items name={trans.title} price={trans.amount} description={trans.description} time={trans.date}/>
-        ))
-      }
+      <div className=" flex flex-col">
+        {transactions?.map((trans) => (
+          <Items
+            name={trans.title}
+            price={trans.amount}
+            description={trans.description}
+            time={trans.date}
+          />
+        ))}
         {/* <Items name={'Shopping'} price={-5120} description={'Buy some groceries'} time={'10:00am'}/>
 
         <Items name={'Food'} price={-530} description={'Arabian Hut'} time={'07:07am'}/>
@@ -51,6 +67,6 @@ export default function transaction() {
 
         <Items name={'Subscription'} price={-1180} description={'Disney and Mobile'} time={'03:07pm'}/> */}
       </div>
-          </div>
-  )
+    </div>
+  );
 }
